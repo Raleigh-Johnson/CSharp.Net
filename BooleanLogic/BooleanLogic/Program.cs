3fbb8fedad330ce3b97d593b2dddf76244b35430
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-25 16:08:34 -0500

    Updated Boolean Logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? true/false");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have??");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualified = (age > 15 && dui != true && tickets <= 3);
+            Console.WriteLine("Are you qualified to drive?");
+            Console.WriteLine(qualified);
+                
+            Console.ReadLine();
+        }
+    }
+}
