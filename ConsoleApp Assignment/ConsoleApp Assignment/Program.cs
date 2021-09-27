abfbef2ff73782c11418a0d6f665135d0da32d98
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-27 12:36:19 -0500

    Console App Assignment1

---
+++ ConsoleApp Assignment/ConsoleApp Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. Input a string to be added to the array.");
+            string newName = Console.ReadLine();
+
+            string[] names = { "Jeff", "Wiley", "Raleigh", "Tulsa" };
+
+            foreach (string name in names)
+            {
+                string addName = (name + newName);
+                Console.WriteLine(addName);
+            }
+
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
