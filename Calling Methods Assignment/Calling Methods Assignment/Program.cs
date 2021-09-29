67907f9410b173ff281d74716f757a61e736f7b5
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-29 13:29:01 -0500

    CallingMethods Upload

---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter a number to be put through the operation.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            operate obj1 = new operate();
+
+            int result1 = obj1.Operation1(number1);
+            int result2 = obj1.Operation2(number1);
+            int result3 = obj1.Operation3(number1);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/operate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class operate
+    {
+
+    
+        public int Operation1(int number1)
+        {
+            int number2 = number1 / 4;
+            return number2;
+        }
+        public int Operation2(int number2)
+        {
+            int number3 = number2 * 8;
+            return number3; 
+        }
+        public int Operation3(int number3)
+        {
+            int result = number3 / 9;
+            return result;
+        }
+
+    }
+}
