d7e1bc3e262c2a51bfea506de6af677cb0345b97
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-30 21:05:29 -0500

    Main Method 2 Upload (Unfinished)

---
+++ MainMethod/Method2/Method2/Operator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Operator
+    {
+        public int Operation1(int userInput1, int userInput2)
+        {
+            int answer1 = userInput1 * 5; //Operator for answer1
+            int answer2 = userInput2 * 5;
+            return answer1;
+        }
+
+    }
+}
---
+++ MainMethod/Method2/Method2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter an integer to be put through the operation.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Enter a second integer (Optional)");
+                int userInput2 = Convert.ToInt32(Console.ReadLine());//User input Int
+            }
+            catch (InvalidProgramException ex)
+            {
+                string userInput2 = Convert.ToInt32(Console.ReadLine());//User input Int
+            }
+            Operator obj1 = new Operator();
+            int answer1 = obj1.Operation1(userInput1,userInput2);
+            Console.WriteLine(answer1);
+
+            Console.ReadLine();
+        }
+    }
+}s
