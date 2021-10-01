60bdf9324cd3f1631f136fa12f9d61703418115f
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-01 16:03:33 -0500

    Class Overloading Methods

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter an integer to be put through the operation.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());//userInput1
+            VoidClass obj1 = new VoidClass();
+            obj1.Operation(userInput1);//Normal Method
+            Console.WriteLine("Hello, enter an decimal to be put through the operation.");
+            decimal userInput2 = Convert.ToDecimal(Console.ReadLine());
+            obj1.Operation(userInput2);//Overloaded Method
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/VoidClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class VoidClass
+    {
+        public void Operation(int userInput1)//"Operation" name of method
+        {
+            int answer1 = userInput1 / 2;//Devides userInput1 by 2
+            Console.WriteLine(answer1);
+        }
+
+        public void Operation(decimal x) //Takes in a decimal
+        {
+            decimal answer1 = x + 5;
+            Console.WriteLine(answer1);
+        }
+
+    }
+}
