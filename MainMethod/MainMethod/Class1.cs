061be06243804301b9f49eb3a58a97946a6f86b7
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-09-30 20:11:18 -0500

    update main method

---
+++ MainMethod/MainMethod/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Class1
+    {
+        public int Operation1(int number1)
+        {
+            int answer1 = number1 * 5;
+            return answer1;
+        }
+
+        public decimal Operation2(decimal number1)
+        {
+            decimal answer2 = number1 * 5;
+            int number2 = Convert.ToInt32(answer2);
+            return answer2;
+        }
+
+        public int Operation3(string number1)
+        {
+            int number3 = Convert.ToInt32(number1);
+            int answer3 = number3 * 5;
+            return answer3;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter an integer to be put through the operation.");
+            int number1 = Convert.ToInt32(Console.ReadLine());//User input Int
+            Class1 obj1 = new Class1();
+            int answer1 = obj1.Operation1(number1);
+            Console.WriteLine(answer1);//Writes answer after being affected by operator
+
+
+            Console.WriteLine("Hello, enter a decimal to be put through the operation.");
+            decimal number2 = Convert.ToDecimal(Console.ReadLine());//Converts string to decimal
+            decimal answer2 = obj1.Operation2(number2);
+            Console.WriteLine(answer2);//Writes answer after being affected by operator
+
+            Console.WriteLine("Hello, enter a string to be put through the operation.");
+            string number3 = Console.ReadLine();
+            int answer3 = obj1.Operation3(number3);
+            Console.WriteLine(answer3);//Writes answer after being affected by operator
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
