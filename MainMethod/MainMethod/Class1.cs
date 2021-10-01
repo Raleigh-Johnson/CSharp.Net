9781fdddb81a365a3c108202c5ba2201b508c4bb
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-01 13:00:42 -0500

    Updated working Method 1&2

--- MainMethod/MainMethod/Class1.cs
+++ MainMethod/MainMethod/Class1.cs
@@ -10,20 +10,20 @@ namespace MainMethod
     {
         public int Operation1(int number1)
         {
-            int answer1 = number1 * 5;
+            int answer1 = number1 * 5; //Operator for answer1
             return answer1;
         }
 
-        public decimal Operation2(decimal number1)
+        public decimal Operation1(decimal number1)
         {
             decimal answer2 = number1 * 5;
-            int number2 = Convert.ToInt32(answer2);
+            int number2 = Convert.ToInt32(answer2);//Converts from decimal to integer to be returned
             return answer2;
         }
 
-        public int Operation3(string number1)
+        public int Operation1(string number1)
         {
-            int number3 = Convert.ToInt32(number1);
+            int number3 = Convert.ToInt32(number1);//Converts string to integer
             int answer3 = number3 * 5;
             return answer3;
         }
