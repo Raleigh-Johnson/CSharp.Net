49ec77a2197062b65892887d80e4d08a68a72d85
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-01 14:12:36 -0500

    Main Method3 finished

---
+++ MainMethod3/MainMethod3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, enter an integer to be put through the operation.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());//userInput1
+            Console.WriteLine("Hello, enter an integer to be put through the operation.");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());//userInput2
+            VoidClass obj1 = new VoidClass();
+            obj1.Operation(userInput1, userInput2);
+            obj1.Operation(userInput1: 3,userInput2: 4);
+
+            Console.ReadLine();
+
+
+        
+        }
+    }
+}
+    
+
---
+++ MainMethod3/MainMethod3/VoidClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod3
+{
+    class VoidClass
+    {
+            public void Operation(int userInput1, int userInput2)
+        {
+            int answer1 = userInput1 * 4;
+            int answer2 = userInput2;
+            Console.WriteLine(answer2);
+        }
+    }
+}
