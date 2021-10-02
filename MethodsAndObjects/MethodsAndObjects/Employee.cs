fab530844fc8867c1cd7e4227e19c2daf2a828c7
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-02 15:23:20 -0500

    Unfinished Methods and Objects

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Employee  // Base class
+    {
+        public Person : Employee()
+        {
+            int ID = 0451;
+
+
+        }
+
+    }       
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+
+        class Persons : Person  // Derived class
+        {
+            public string FirstName = "Jeff";  // LastName field
+
+            public string LastName = "Wiley";  // LastName field
+
+            public void SayName()             // SayName method 
+            {
+                Console.WriteLine(FirstName + LastName);
+            }
+            
+        }
+
+    }   
+}
