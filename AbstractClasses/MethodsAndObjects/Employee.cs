5a93505d359be35e328778d6ead226c3e463414f
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-02 17:29:55 -0500

    Abstract assignment

---
+++ AbstractClasses/MethodsAndObjects/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    
+    public class Employee : Person  // Base class
+    {
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+
+     
+          
+    }       
+}
---
+++ AbstractClasses/MethodsAndObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee(); //Substantiate Object
+            Emp1.FirstName = "Sample"; //Assigns a value to the FirstName property of the object
+            Emp1.LastName = "Student"; //Assigns a value to the LastName property of the object
+            Emp1.SayName();  // Calls the "SayName" Method through the object.
+            Console.ReadLine();
+        }
+    }
+}
