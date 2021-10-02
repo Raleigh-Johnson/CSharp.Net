e8113af771b3ed6ae234cb8bcd64f6adbdbef2f8
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-02 18:56:16 -0500

    Abstract class assignment ReUpload

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program : Employee
+    {
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee Emp1 = new Employee(); //instantiate Object
+            Emp1.FirstName = "Sample"; //Assigns a value to the FirstName property of the object
+            Emp1.LastName = "Student"; //Assigns a value to the LastName property of the object
+            Emp1.SayName();  // Calls the "SayName" Method through the object.
+            Console.ReadLine();
+        }
+    }
+}
