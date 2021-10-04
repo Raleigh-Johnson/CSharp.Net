6c354c7c07a3c4750afb3c248dfd5203bc885106
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-04 16:03:18 -0500

    Lambda Unfinished

---
+++ ConsoleApp1/ConsoleApp1/Lambacs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            struct Employee
+            {
+
+                public int EmpID;
+                public string FirstName;
+                public string LastName;
+                public string ssn;
+            }
+
+        Employee emp0;
+        emp0.EmpID = 1234;
+            emp0.FirstName = "Jeff";
+            emp0.LastName = "Wiley";
+            emp0.ssn = "123-45-6789";
+            Employee emp1;
+        Employee emp2;
+        Employee emp3;
+        Employee emp4;
+        Employee emp5;
+        Employee emp6;
+        Employee emp7;
+        Employee emp8;
+        Employee emp9;
+
+        emp0.EmpID = 1234;
+            emp1.FirstName = "";
+            emp1.LastName = "";
+            emp1.ssn = "";
+
+            List<string> Emp = new List<string>();
+
+
+
+
+
+    }
+            
+
+        
+    }
+}
---
+++ LambdaAndLists/LambdaAndLists/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAndLists
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+                 
+
+            
+            
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { FirstName = "Joe", LastName = "Wiley", ID = 0 },
+                new Employee { FirstName = "Mikah", LastName = "Anders", ID = 1 },
+                new Employee { FirstName = "Eve", LastName = "Wiatt", ID = 2 },
+                new Employee { FirstName = "Andrew", LastName = "Sandches", ID = 3 },
+                new Employee { FirstName = "Joe", LastName = "Jefferson", ID = 4 },
+                new Employee { FirstName = "Zepher", LastName = "Crosby", ID = 5 },
+                new Employee { FirstName = "Penelope", LastName = "Jacobs", ID = 6 },
+                new Employee { FirstName = "Eric", LastName = "Rodrigues", ID = 7 },
+                new Employee { FirstName = "David", LastName = "Amber", ID = 8 },
+                new Employee { FirstName = "Sarah", LastName = "Weisbrodt", ID = 9 }
+            };
+
+            List<Employee> EmployeeList = employees.FindAll(x => x.FirstName == "Joe");
+            Console.WriteLine(employees);
+            //foreach (Employee employee in employees )
+            //{
+            //    Console.WriteLine(employee);
+            //}
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
