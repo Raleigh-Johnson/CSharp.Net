587910a888abdcc78e3d742381fd25e608399196
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-06 13:40:42 -0500

    Date&Time

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DT1 = DateTime.Now;
+            Console.WriteLine(DT1); //Prints the current time
+            Console.ReadLine();
+            Console.WriteLine("Please input a number"); //Asks the user for an integer input
+            float Input = float.Parse(Console.ReadLine());// assigns the users input to "Input"
+            DateTime TimeUser = DT1.AddHours(Input);
+            Console.WriteLine("In {0} Hours, the time will read {1}",Input, TimeUser); //DateTime.Hour
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
