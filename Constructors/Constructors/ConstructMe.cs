5a1d0cdfa7c528471b0416b277f6679d48debcd2
Raleigh Johnson <84159459+Raleigh-Johnson@users.noreply.github.com>
2021-10-07 20:31:55 -0500

    Construct chaining

---
+++ Constructors/Constructors/ConstructMe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class ConstructMe
+    { //Identifies proporties 
+        public string name;
+        public int id;
+        public ConstructMe(string Name, int id)
+        {
+            this.name = Name;
+            this.id = id;
+        }
+        
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program : Person
+    { 
+        public static void Main()
+        {
+            Person Person1 = new Person(); //Defines Construct
+           
+            var i = 5;
+            Console.WriteLine(i);
+
+            ConstructMe a = new ConstructMe("Raleigh ", 16);
+            Console.WriteLine($"The instance with the name: { nameof(Person1)}, has property name: {a.name} and id {a.id}");
+            Console.ReadLine();
+        }
+    }
+}
+
