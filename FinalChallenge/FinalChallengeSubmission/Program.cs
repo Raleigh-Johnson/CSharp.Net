4dbfaf514caeeffedec7f9835d7b3c29909cebcc
Raleigh Johnson <r.aleigh.j55@gmail.com>
2021-10-23 16:24:23 -0500

    Final Challenge upload

---
+++ FinalChallenge/FinalChallengeSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallengeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ( var db = new SchoolContext())
+            {
+                var student = new Student()
+                {
+                    StudentName = "Henry Rodgers",
+                    EmailAddress = "Raleighj.dev@gmail.com"
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from b in db.Students
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
